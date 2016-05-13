3dd90280d052473e1c185bc9e5593fda2af28a3d
Dwayne Linde <lindesara@gmail.com>
2016-05-13 13:35:25 -0700

    Assignment 6

---
+++ functions-old.php
@@ -0,0 +1,41 @@
+<?php 
+/* 
+Theme Name: Dwayneland Spring 2016 Demo
+Author: Dwayne Linde
+Author URI: http://lindese.com/web170/
+Description: The Spring 2016 WordPress class site for Dwayne Linde. 
+Version: 0.0.01
+*/
+
+// This is where the main navigation menu lives. 
+
+// register_nav_menus( array (
+// 'main-menu' => __('Main'), 
+// )); 
+    
+
+// This is where my menu lives. 
+
+function register_my_menu() {
+    register_nav_menu('main-menu',__( 'Main Menu')); 
+}
+add_action( 'init', 'register_my_menu' ); 
+  
+// This is where the sidebar menu lives. 
+// Info here: https://developer.wordpress.org/reference/functions/wp_list_pages/#source-code
+
+function wp_list_pages ( $args = '' ) { 
+    $defaults = array( 
+        'depth' => 0, 'show_date' => '', 
+        'date_format' => get_option( 'date_format' ), 
+        'child_of' => 0, 'exclude' => '', 
+        'title_li' => __( 'Pages' ), 'echo' => 1, 
+        'authors' => '', 'sort_column' => 'menu_order, post_title', 
+        'link_before' => '', 'link_after' => '', 'walker' => '', 
+        ); 
+
+    $r = wp_parse_args( $args, $defaults ); 
+}
+
+        
+?>
--- header.php
+++ header.php
@@ -8,24 +8,23 @@
 <link href='https://fonts.googleapis.com/css?family=Delius+Swash+Caps' rel='stylesheet' type='text/css'>
 
 <link rel="stylesheet"  href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all"/>
-    
 </head>
     
 <body>
 <header> 
-    <img src="<?php bloginfo('template_directory'); ?>/images/cornerstone_placeholder.gif" class="icon" alt="A placeholder cornerstone image. Labeled for reuse." />
+<img src="<?php bloginfo('template_directory'); ?>/images/cornerstone_placeholder.gif" class="icon" alt="A placeholder cornerstone image. Labeled for reuse." />
     
     <!--<img src="icons/Logo-Black-41px-TM.png" class="social" alt="LinkedIn link icon" />-->
     
     
-    <a href="https://www.linkedin.com/in/test" target="_blank">
+<a href="https://www.linkedin.com/in/test" target="_blank">
 <img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_viewmy_160x25.png" width="160" height="25" border="0" class="social" alt="View Aimee Elber's profile on LinkedIn.">
-        </a>
+</a>
     
     
-    <a href="mailto:example@example.com ">
-    <img src="<?php bloginfo('template_directory'); ?>/icons/email.png" class="email" alt="Email link icon." />
-    </a>
+<a href="mailto:example@example.com ">
+<img src="<?php bloginfo('template_directory'); ?>/icons/email.png" class="email" alt="Email link icon." />
+</a>
         
      
      <!-- <h2 class="pageid">Home Page</h2> -->
@@ -37,6 +36,13 @@
     <!-- Here is the nav menu, via calling the wp_nav_menu() function --> 
     <!-- The function is actually broken down and stores in 'functions.php'; this calls it. --> 
     
-    <?php wp_nav_menu( array( 'Theme_location' => 'main-menu' ) ); ?> 
+<nav>
+<ul class="nav">
+<?php wp_nav_menu( array( 'Theme_location' => 'main-menu' ) ); ?> 
+</ul>  
+</nav>
+    
+    
+    
     
     <!-- end header --> 
