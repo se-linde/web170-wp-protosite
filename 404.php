a0152c092abc56822a3eef75b2c4f75ced2ff31c
Dwayne Linde <lindesara@gmail.com>
2016-07-14 15:21:48 -0700

    Wrote a 404.php page, in case someone gets lost.
    It also incorporates a search form function, but that doesn’t work yet.

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+    <!-- Begin Content --> 
+
+    <div id="content">
+    <h2>404 Error:</h2>    
+
+        <p>The page you are looking for doesn't exist. Try a search? </p>
+        <?php get_search_form(); ?> 
+    <small>404.php</small>
+    </div>
+
+    <!-- End Content --> 
+
+<?php get_sidebar(); ?> 
+<?php get_footer(); ?> 
+
+
+
