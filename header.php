872805739726b7e08b7d96ee57a9a074f799d44b
Dwayne Linde <lindesara@gmail.com>
2016-08-02 16:21:58 -0700

    Updated header.
    Pray for me.

--- header.php
+++ header.php
@@ -1,49 +1,105 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">    
 <title><?php get_my_title_tag(); ?></title>
     
 <!-- begin meta --> 
     
-    
-    
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width" />
-<meta name="description" content="<?php echo get_the_excerpt(); ?>">  
-    
-<link href='https://fonts.googleapis.com/css?family=Delius+Swash+Caps' rel='stylesheet' type='text/css'>
+<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0" />
+<meta name="description" content="<?php echo get_the_excerpt(); ?>" />
 
-<link rel="stylesheet"  href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all"/>
-</head>
-    
-<body>
-<header> 
-<!--<img src="<?php bloginfo('template_directory'); ?>/images/cornerstone_placeholder.gif" class="icon" alt="A placeholder cornerstone image. Labeled for reuse." />--> 
-    
-    
-    
-<a href="https://www.linkedin.com/in/test" target="_blank">
-<img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_viewmy_160x25.png" width="160" height="25" border="0" class="social" alt="View Aimee Elber's profile on LinkedIn.">
-</a>
-    
-    
-<a href="mailto:example@example.com ">
-<img src="<?php bloginfo('template_directory'); ?>/icons/email.png" class="email" alt="Email link icon." />
-</a>
-        
-     
-     <!-- <h2 class="pageid">Home Page</h2> -->
-    
-  <h3><a href="index.php">Cornerstone Consulting</a></h3>
-  <!-- START NAVIGATION (Main) -->
-
-    
-    <!-- Here is the nav menu, via calling the wp_nav_menu() function --> 
-    <!-- The function is actually broken down and stores in 'functions.php'; this calls it. --> 
-    
-<?php wp_nav_menu( array( 'Theme_location' => 'main-menu', 'container' => 'div', 
-/* 'container_id' => 'navigation' */ /* Will match up with CSS tags.*/ )); ?> 
-
+<!-- end meta --> 
         
     
-    <!-- end header --> 
+<!-- begin links --> 
+
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/flexslider.css" type="text/css" />
+                                                                                             
+    
+<!-- end links --> 
+ 
+<!-- begin scripts --> 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/scripts/jquery.flexslider.js"></script>
+    
+<!-- end scripts --> 
+
+    
+<!-- begin FlexSlider --> 
+
+<script type="text/javascript" charset="utf-8"> 
+
+    $(window).load(function() { // enable function upon window load
+        $('.flexslider').flexslider(); // call FlexSlider function
+    }); 
+    
+</script>    
+    
+<!-- end FlexSlider -->     
+    
+<!-- begin Toggle Menu --> 
+
+<script type="text/javascript" charset="utf-8"> 
+
+    $(window).load(function() { // enable function upon window load
+        $("#toggle").click(function() { // When toggle is clicked....
+            $("#navigation").toggle(); // ... open or close the navigation. 
+        }); 
+    }); 
+    
+</script>    
+    
+<!-- end Toggle Menu -->     
+    
+<!-- begin WP Head function --> 
+<? php wp_head(); ?>
+<!-- end WP Head function --> 
+        
+</head>    
+
+<body <?php body_class(); ?>> 
+    
+<!-- begin header --> 
+    
+<div id="header">
+<h1 id="logo"><a href="<?php echo get_settings('home'); ?>">Sara Dwayne Linde</a></h1>    
+<img id="toggle" src="<?php bloginfo('template_directory'); ?>/images/img-toggle.png" width="25" height="25" alt="Toggle Menu">   
+    
+</div>    
+    
+<!-- end header --> 
+    
+<!-- Begin Navigation -->
+<!-- <div id="navigation">
+<ul id="navigation-items">
+<li><a href="main.html">About</a></li>
+<li><a href="main.html">Portfolio</a></li>
+<li><a href="main.html">Blog</a></li>
+<li><a href="main.html">Contact</a></li>
+</ul> 
+</div> -->    
+    
+<? php wp_nav_menu(array('theme_location' => 'main_menu', 'container' => 'div', 'container_id' => 'navigation',)); ?>     
+      
+    
+<!-- End Navigation -->
+
+<!-- Begin Content -->
+<div id="middle">    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
