9fe020115fc902b7d7db9fdbae2ee7edd9378c8a
Dwayne Linde <lindesara@gmail.com>
2016-09-11 01:20:46 -0700

    Updated front page.

--- front-page-old2.php
+++ front-page-old2.php
@@ -1,18 +1,8 @@
 <?php get_header(); ?> 
 
-
-<!-- Begin Flex Slider --> 
-
-
-<!-- Put In Flex Slider --> 
-
-
-<!-- End Fles Slider --> 
-
-
 <!-- Main Content --> 
 
-<div id="widgets">
+<div id="content">
     <section class="widget item"> 
     <?php 
      // This is the loop that feeds the main post content to the page.     
@@ -25,27 +15,34 @@
      <small>front-page.php</small>
      </section>
     
-    
     <section class="widget item">
     
     <h2>Latest Postings: </h2>
+    <ul>
         <?php rewind_posts(); // This stops Loop 1 from looping. Widget 2, Loop 2. ?>
-        <?php query_posts('showposts=5'); // Dear Loop 2: show last 5 posts. ?>
-        <?php if (have_posts()) : while (have _posts()) : the_post(); // Start Loop 2. ?> 
+        <?php query_posts('showposts=4'); // Dear Loop 2: show last 4 posts. ?>
+        <?php while (have_posts()) : the_post(); // Start Loop 2. ?> 
         <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-        <?php endwhile; endif; // This ends Loop 2. ?> 
-
+        <?php endwhile; // This ends Loop 2. ?> 
+    </ul>
     </section>   
     
     <section class="widget item">
     <!-- This is static HTML --> 
         
-    <h2>Contact Me:</h2>
-        <p><strong>Email: </strong><a href="mailto:lindesara@gmail.com">lindesara@gmail.com</a></p>
+    <ul>
+   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+   <li>Aliquam tincidunt mauris eu risus.</li>
+   <li>Vestibulum auctor dapibus neque.</li>
+    </ul>    
+        
     </section>    
+        
         
 </div>     
      
+<?php get_sidebar(); ?> 
+
 <!-- content ends --> 
 
 <?php get_footer(); ?> 
